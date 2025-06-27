639db84e8082fcc94f1476cbf2a2f45e7013ff91
EnzoFanAccount <enzobiondo11@outlook.com>
2025-06-27 07:49:19 -0300

    bump docs

--- source/conf.py
+++ source/conf.py
@@ -9,7 +9,7 @@
 project = 'SecureML'
 copyright = '2025, Enzo Biondo'
 author = 'Enzo Biondo'
-release = '0.3.0'
+release = '0.3.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
